d3775c5db803db4b0ffeffffb41be0b7d3653f82
Adil Koondhar <146661723+adilkoondhar@users.noreply.github.com>
2024-02-11 23:04:29 +0500

    Add update student api route

---
+++ app/api/students/add/route.ts
@@ -0,0 +1,62 @@
+import bcrypt from 'bcryptjs';
+import User from '@/models/Student'; // Adjust the path according to your project structure
+import dbConnect from '@/utils/dbConnect';
+import {NextRequest, NextResponse} from "next/server";
+
+export async function POST(req: NextRequest) {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const image = body.image;
+    const firstName = body.firstName;
+    const lastName = body.lastName;
+    const email = body.email;
+    const password = body.password;
+    const phoneNumber = body.phoneNumber;
+    const course = body.course;
+
+    if (req.url && req.url.endsWith('/students/add')) {
+
+        try {
+            // Check if user already exists
+            const existingUser = await User.findOne({email});
+            if (existingUser) {
+                return NextResponse.json({error: 'Student already exists'}, {
+                    status: 400
+                });
+            }
+
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Create a new user
+            const newUser = new User({
+                image,
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                phoneNumber,
+                course,
+                fullName: `${firstName} ${lastName}`
+            });
+
+            // Save the new user
+            await newUser.save();
+
+            // Return the JWT token
+            return NextResponse.json({
+                message: 'Student added successfully'
+            }, {
+                status: 200
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        return NextResponse.json({ error: `Route not found + ${req.url}` }, {
+            status: 404
+        });
+    }
+}
---
+++ app/api/students/update/route.ts
@@ -0,0 +1,58 @@
+import bcrypt from 'bcryptjs';
+import User from '@/models/Student'; // Adjust the path according to your project structure
+import dbConnect from '@/utils/dbConnect';
+import {NextRequest, NextResponse} from "next/server";
+
+export async function POST(req: NextRequest) {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const id = body.id;
+    const image = body.image;
+    const firstName = body.firstName;
+    const lastName = body.lastName;
+    const email = body.email;
+    const password = body.password;
+    const phoneNumber = body.phoneNumber;
+    const course = body.course;
+
+    if (req.url && req.url.endsWith('/students/update')) {
+
+        try {
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Update the user
+            const updatedUser = await User.findByIdAndUpdate(id, {
+                image,
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                phoneNumber,
+                course,
+                fullName: `${firstName} ${lastName}`
+            }, {new: true}); // new: true returns the updated document
+
+            if (!updatedUser) {
+                return NextResponse.json({error: 'Student not found'}, {
+                    status: 404
+                });
+            }
+
+            // Return the success message
+            return NextResponse.json({
+                message: 'Student updated successfully'
+            }, {
+                status: 200
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        return NextResponse.json({ error: `Route not found + ${req.url}` }, {
+            status: 404
+        });
+    }
+}
