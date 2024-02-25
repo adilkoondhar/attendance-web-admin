3f7565fd620ece66fa28cea0cbc8abfad183eb86
Adil Koondhar <146661723+adilkoondhar@users.noreply.github.com>
2024-02-25 23:14:51 +0500

    Fix update API error

--- app/api/students/update/route.ts
+++ app/api/students/update/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: NextRequest) {
             const hashedPassword = await bcrypt.hash(password, 10);
 
             // Update the user
-            const updatedUser = await User.findByIdAndUpdate(id, {
+            const updatedUser = await User.findOneAndUpdate({ id }, {
                 image,
                 firstName,
                 lastName,
